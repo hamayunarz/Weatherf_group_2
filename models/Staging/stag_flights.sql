a6f89c4347155468548a4f01581b497522656752
Group 2 <dataweather92@gmail.com>
2025-05-20 06:50:07 +0000

    s

--- models/Staging/stag_flights.sql
+++ models/Staging/stag_flights.sql
@@ -1,1 +1,22 @@
-SELECT * FROM {{ source('group_2_project_weather', 'flights_all') }}
+WITH source_flights AS (
+    SELECT * FROM {{ source('group_2_project_weather', 'flights_all') }}
+)
+
+SELECT
+    flight_date,
+    origin,
+    dest,
+    airline,
+    tail_number,
+    flight_number,
+    dep_time,
+    sched_dep_time,
+    dep_delay,
+    arr_time,
+    sched_arr_time,
+    arr_delay,
+    cancelled,
+    diverted,
+    distance,
+    actual_elapsed_time
+FROM source_flights
